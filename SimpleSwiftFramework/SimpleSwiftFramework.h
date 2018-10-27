bd25915df70b1c0fe290f198f346767d1078d5ef
MaDonRa <bank-681@hotmail.com>
2018-10-27 13:17:27 +0700

    Initial Commit

---
+++ SimpleSwiftFramework/SimpleSwiftFramework.h
@@ -0,0 +1,19 @@
+//
+//  SimpleSwiftFramework.h
+//  SimpleSwiftFramework
+//
+//  Created by Sakkaphong Luaengvilai on 27/10/2561 BE.
+//  Copyright © 2561 Sakkaphong Luaengvilai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SimpleSwiftFramework.
+FOUNDATION_EXPORT double SimpleSwiftFrameworkVersionNumber;
+
+//! Project version string for SimpleSwiftFramework.
+FOUNDATION_EXPORT const unsigned char SimpleSwiftFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SimpleSwiftFramework/PublicHeader.h>
+
+
